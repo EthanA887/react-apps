da768dfe956966a4752ac45cdfa3e1ed1ca7351c
EthanA887 <ethan_a@mail.com>
2024-09-04 12:51:00 -0400

    First commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,27 @@
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <>
+      <div>
+        <Alert>
+          Hello, <span>world!</span>
+        </Alert>
+        <ListGroup
+          items={items}
+          heading="Cities"
+          onSelectItem={handleSelectItem}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ react-exercise/src/App.tsx
@@ -0,0 +1,18 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>button</Button>
+    </div>
+  );
+}
+
+export default App;
